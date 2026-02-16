69c87899c22da57453add6846f8078846b3009cf
kevin <yangyongrui0402@gmail.com>
2026-02-17 01:39:40 +0800

    調整路徑

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// 如果您未來使用 www.xn--ht0a.tw 且對應到根目錄，請將 base 改回 '/'
 export default defineConfig({
+  base: '/horse_red_packet_2026/',
   plugins: [react()],
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
