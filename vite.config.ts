a42f39cd62bbeed567c5a1bfb82e788598d071da
kevin <yangyongrui0402@gmail.com>
2026-02-17 02:12:47 +0800

    修正路徑問題

--- vite.config.ts
+++ vite.config.ts
@@ -11,11 +11,10 @@ export default defineConfig(({ mode }) => {
 
   return {
     // 這裡必須對應您的 GitHub 儲存庫名稱（子目錄路徑）
-    // 如果未來使用 www.xn--ht0a.tw 且對應到根目錄，請將此改為 '/'
     base: '/horse_red_packet_2026/',
     plugins: [react()],
     define: {
-      // 這行非常重要，它會將 API_KEY 直接替換進打包後的代碼
+      // 這行會將 API_KEY 直接替換進打包後的代碼
       'process.env.API_KEY': JSON.stringify(apiKey)
     },
     build: {
